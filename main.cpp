81770647a6745bf1370c2a2c79333c5d4606a438
Adam Boardman <adamboardman@gmail.com>
2018-01-07 14:35:28 +0000

    Cannot decide if this is useful or not.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     //app.setFont(QFont{"Noto Sans", app.font().pointSize(), QFont::Normal});
 
-    app.setAttribute(Qt::AA_EnableHighDpiScaling);
+    //app.setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QQmlApplicationEngine engine("qml/main.qml");
     return app.exec();
